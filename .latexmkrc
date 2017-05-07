598efaa8f984f983ea2c7625c37106dcc3ad9412
Seiya-Yamamoto <linux.yamagen@gmail.com>
2017-05-08 05:56:53 +0900

    modified

--- .latexmkrc
+++
@@ -1,3 +0,0 @@
-$latex='platex -kanji=utf8 -guess-input-enc -synctex=1 -interaction=nonstopmode %S';
-$dvipdf='dvipdfmx  %S';
-$bibtex='pbibtex -kanji=utf8 %B';
--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 ln -sf /home/yamagen/dotfiles/nvim/ /home/yamagen/.config/
+ln -sf /home/yamagen/dotfiles/dein/ /home/yamagen/.config/
 ln -sf /home/yamagen/dotfiles/.bashrc /home/yamagen/.bashrc
-ln -sf /home/yamagen/dotfiles/.zshrc /home/yamagen/.zshrc
-ln -sf /home/yamagen/dotfiles/.latexmkrc /home/yamagen/.latexmkrc
