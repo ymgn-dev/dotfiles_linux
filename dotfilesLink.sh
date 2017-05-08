71e767355fbe787d082f436a9e859039ee5079fa
Seiya Yamamoto <rione.yamagen@gmail.com>
2017-05-09 02:23:21 +0900

    dotfilesLink.sh removed

--- dotfilesLink.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-ln -sf /home/yamagen/dotfiles/nvim/ /home/yamagen/.config/
-ln -sf /home/yamagen/dotfiles/dein/ /home/yamagen/.config/
-ln -sf /home/yamagen/dotfiles/.bashrc /home/yamagen/.bashrc
