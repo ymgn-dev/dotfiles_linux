a82aee235e8a6a14e86253ac480703e989aebb62
Seiya Yamamoto <rione.yamagen@gmail.com>
2017-05-09 02:21:17 +0900

    link, unlink added

---
+++ link.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+ln -sf /home/yamagen/dotfiles/nvim/ /home/yamagen/.config/nvim
+ln -sf /home/yamagen/dotfiles/dein/ /home/yamagen/.config/dein
+ln -sf /home/yamagen/dotfiles/.bashrc /home/yamagen/.bashrc
---
+++ unlink.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+unlink /home/yamagen/.config/nvim
+unlink /home/yamagen/.config/dein
+unlink /home/yamagen/.bashrc
