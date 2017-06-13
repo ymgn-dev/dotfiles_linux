e894ee396935601a66fea178092119a3bbfaef2f
Seiya Yamamoto <linux.yamagen@gmail.com>
2017-06-13 22:53:23 +0900

    link.sh update

--- link.sh
+++ link.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-ln -sf ${HOME}/dotfiles/nvim/ /home/yamagen/.config/nvim
-ln -sf ${HOME}/dotfiles/.bashrc /home/yamagen/.bashrc
+ln -sf ${HOME}/dotfiles/nvim/ ${HOME}/.config/nvim
+ln -sf ${HOME}/dotfiles/.bashrc ${HOME}/.bashrc
 ln -sf ${HOME}/dotfiles/inputrc /etc/inputrc
