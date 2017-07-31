d3e39c8bfa3b3f80e9ce99a00e656a546ab1e1b4
Seiya Yamamoto <rione.yamagen@gmail.com>
2017-08-01 03:17:58 +0900

    link.sh unlink.sh update

--- link.sh
+++ link.sh
@@ -1,4 +1,25 @@
 #!/bin/sh
-ln -sf ${HOME}/dotfiles/nvim/ ${HOME}/.config/nvim
-ln -sf ${HOME}/dotfiles/.bashrc ${HOME}/.bashrc
-ln -sf ${HOME}/dotfiles/inputrc /etc/inputrc
+
+SCRIPT_DIR=$(cd $(dirname $0); pwd)
+
+sudo ln -sf $SCRIPT_DIR/.bashrc ${HOME}/.bashrc
+sudo ln -sf $SCRIPT_DIR/inputrc /etc/inputrc
+
+# Neovim Installing Check
+if type nvim 2>/dev/null 1>/dev/null
+then
+	echo "nvim found !"
+	ln -sf $SCRIPT_DIR/dotfiles/nvim/ ${HOME}/.config/nvim
+else
+	echo "nvim not found !"
+fi
+
+# Vim Installing Check
+if type vim 2>/dev/null 1>/dev/null
+then
+	echo "vim found !"
+	sudo ln -sf $SCRIPT_DIR/.vimrc ${HOME}/.vimrc
+	ln -sf $SCRIPT_DIR/dotfiles/.vim/ ${HOME}/.vim
+else
+	echo "vim not found !"
+fi
--- unlink.sh
+++ unlink.sh
@@ -1,4 +1,20 @@
 #!/bin/sh
-unlink ${HOME}/.config/nvim
 unlink ${HOME}/.bashrc
 unlink /etc/inputrc
+
+# Neovim Installing Check !
+if [ -e "${HOME}/.config/nvim" ]; then
+    echo "nvim found. unlinked !"
+    unlink ${HOME}/.config/nvim
+else
+    echo "nvim not found."
+fi
+
+# Vim Installing Check !
+if [ -e "${HOME}/.vim" ]; then
+    echo "Vim found. unlinked !"
+    unlink ${HOME}/.vim
+    unlink ${HOME}/.vimrc
+else
+    echo "nvim not found."
+fi
