4fbcb7377d618db69a0e32c166c9f8cf7da106c5
Seiya Yamamoto <linux.yamagen@gmail.com>
2018-07-04 18:17:31 +0900

    modified link.sh unlink.sh

--- link.sh
+++ link.sh
@@ -2,24 +2,27 @@
 
 SCRIPT_DIR=$(cd $(dirname $0); pwd)
 
+echo ".bashrc linked"
 sudo ln -sf $SCRIPT_DIR/.bashrc ${HOME}/.bashrc
+
+echo "inputrc linked"
 sudo ln -sf $SCRIPT_DIR/inputrc /etc/inputrc
 
 # Neovim Installing Check
 if type nvim 2>/dev/null 1>/dev/null
 then
-	echo "nvim found ! linked"
+	echo "nvim found! linked"
 	ln -sf $SCRIPT_DIR/nvim/ ${HOME}/.config/nvim
 else
-	echo "nvim not found !"
+	echo "nvim not found!"
 fi
 
 # Vim Installing Check
-if type vim 2>/dev/null 1>/dev/null
-then
-	echo "vim found ! linked"
-	sudo ln -sf $SCRIPT_DIR/.vimrc ${HOME}/.vimrc
-	ln -sf $SCRIPT_DIR/vim/ ${HOME}/.vim
-else
-	echo "vim not found !"
-fi
+# if type vim 2>/dev/null 1>/dev/null
+# then
+# 	echo "vim found ! linked"
+# 	sudo ln -sf $SCRIPT_DIR/.vimrc ${HOME}/.vimrc
+# 	ln -sf $SCRIPT_DIR/vim/ ${HOME}/.vim
+# else
+# 	echo "vim not found !"
+# fi
--- unlink.sh
+++ unlink.sh
@@ -1,20 +1,24 @@
 #!/bin/sh
+
+echo ".bashrc unlinked"
 sudo unlink ${HOME}/.bashrc
+
+echo "inputrc unlinked"
 sudo unlink /etc/inputrc
 
 # Neovim Installing Check !
 if [ -e "${HOME}/.config/nvim" ]; then
-    echo "nvim found. unlinked !"
+    echo "nvim found. unlinked!"
     sudo unlink ${HOME}/.config/nvim
 else
     echo "nvim not found."
 fi
 
 # Vim Installing Check !
-if [ -e "${HOME}/.vim" ]; then
-    echo "Vim found. unlinked !"
-    sudo unlink ${HOME}/.vim
-    sudo unlink ${HOME}/.vimrc
-else
-    echo "nvim not found."
-fi
+# if [ -e "${HOME}/.vim" ]; then
+#     echo "Vim found. unlinked !"
+#     sudo unlink ${HOME}/.vim
+#     sudo unlink ${HOME}/.vimrc
+# else
+#     echo "nvim not found."
+# fi
